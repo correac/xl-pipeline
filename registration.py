5a4f4101b245b99b5c6a22bcabfb0b5f063d2bf7
Josh Borrow <joshua.borrow@durham.ac.uk>
2020-03-24 16:59:03 +0000

    Added metallicity registration functions

--- registration.py
+++ registration.py
@@ -6,9 +6,15 @@ This file calculates:
     + sSFR (30, 100 kpc) (specific_sfr_gas_{x}_kpc)
         This is the specific star formation rate of gas within those apertures.
         Only calculated for galaxies with a stellar mass greater than zero.
-    + is_passive (30, 100 kpc) (is_passive_{x}_kpc)
+    + is_passive and is_active (30, 100 kpc) (is_passive_{x}_kpc)
         Boolean that determines whether or not a galaxy is passive. Marginal
         specific star formation rate is 1e-11 year^-1.
+    + 12 + log(O/H) ({gas_twelve_plus_log_OH_{x}_kpc, 30, 100 kpc)
+        12 + log(O/H) based on metallicities. These should be removed at some point
+        once velociraptor has a more sensible way of dealing with metallicity
+        units.
+    + metallicity_in_solar (star_metallicity_in_solar_{x}_kpc, 30, 100 kpc)
+        Metallicity in solar units (relative to metal_mass_fraction).
 """
 
 aperture_sizes = [30, 100]
@@ -41,3 +47,44 @@ for aperture_size in aperture_sizes:
     setattr(self, f"specific_sfr_gas_{aperture_size}_kpc", ssfr)
     setattr(self, f"is_passive_{aperture_size}_kpc", is_passive)
     setattr(self, f"is_active_{aperture_size}_kpc", is_active)
+
+
+# Now metallicities relative to different units
+
+solar_metal_mass_fraction = 0.0126
+twelve_plus_log_OH_solar = 8.69
+
+for aperture_size in aperture_sizes:
+    try:
+        metal_mass_fraction_star = (
+            getattr(catalogue.apertures, f"zmet_star_{aperture_size}_kpc")
+            / solar_metal_mass_fraction
+        )
+        metal_mass_fraction_star.name = (
+            f"Star Metallicity $Z_*$ rel. to $Z_\\odot={solar_metal_mass_fraction}$"
+        )
+        setattr(
+            self,
+            f"star_metallicity_in_solar_{aperture_size}_kpc",
+            metal_mass_fraction_star,
+        )
+    except AttributeError:
+        pass
+
+    try:
+        metal_mass_fraction_gas = (
+            getattr(catalogue.apertures, f"zmet_gas_{aperture_size}_kpc")
+            / solar_metal_mass_fraction
+        )
+        log_metal_mass_fraction_gas = np.log10(metal_mass_fraction_gas.value)
+        twelve_plus_log_OH = unyt.unyt_array(
+            twelve_plus_log_OH_solar + log_metal_mass_fraction_gas,
+            units="dimensionless",
+        )
+        twelve_plus_log_OH.name = (
+            f"$12+\\log_{{10}}$O/H from $Z$ (Solar={twelve_plus_log_OH_solar})"
+        )
+
+        setattr(self, f"gas_twelve_plus_log_OH_{aperture_size}_kpc", twelve_plus_log_OH)
+    except AttributeError:
+        pass
