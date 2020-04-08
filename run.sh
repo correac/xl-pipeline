5df178bf6740c153c06e994bd9d5eaf15b63c3c3
Sylvia Ploeckinger <ploeckinger@strw.leidenuniv.nl>
2020-04-08 11:07:20 +0100

    Changes to run.sh

--- run.sh
+++ run.sh
@@ -15,11 +15,11 @@ ptype="COLIBRE"
 
 isnap_formatted=$( printf '%04d' $isnap )
 # First up, an example for running the scripts on one at a time
-run_directory="/cosma7/data/dp004/dc-ploe1/SIMULATION_RUNS/2020_01_COLIBRE_ref/COLIBRE_L006N0188_ref/"
-run_name="COLIBRE_L006N0188_ref"
-plot_directory="Plots"
+run_directory="/cosma7/data/dp004/dc-ploe1/SIMULATION_RUNS/2020_01_COLIBRE_ref/COLIBRE_L006N0188_steepEOS/"
+run_name="COLIBRE_L006N0188_steepEOS"
+plot_directory="Plots_web"
 snapshot_name="data/snaps/colibre_$isnap_formatted.hdf5"
-catalogue_name="data/stf_bh/halos_$isnap_formatted.properties.0"
+catalogue_name="data/stf_hydr/halos_$isnap_formatted.properties.0"
 
 ln -s $run_directory/colibre_*yml $run_directory/data/snaps/
 
@@ -32,20 +32,20 @@ image_run $run_directory $run_name $plot_directory $snapshot_name $catalogue_nam
 
 plot_single_run () {
   run_name=$1
-  run_directory="Runs/${run_name}"
-  plot_directory="Plots"
+  run_directory="/cosma7/data/dp004/dc-ploe1/SIMULATION_RUNS/2020_01_COLIBRE_ref/${run_name}"
+  plot_directory="Plots_web"
   snapshot_name="data/snaps/colibre_$isnap_formatted.hdf5"
-  catalogue_name="data/stf_bh/halos_$isnap_formatted.properties.0"
+  catalogue_name="data/stf_hydr/halos_$isnap_formatted.properties.0"
 
   mkdir -p $plot_directory/$run_name
 
   # Check if run exists before doing any of this stuff!
   if [[ -f $run_directory/$snapshot_name ]]
   then
-    plot_run $run_directory $run_name $plot_directory $snapshot_name $catalogue_name
-    create_summary_plot $run_directory $run_name $plot_directory $snapshot_name $catalogue_name
+    plot_run $run_directory $run_name $plot_directory $snapshot_name $catalogue_name $ptype
+    create_summary_plot $run_directory $run_name $plot_directory $snapshot_name $catalogue_name $ptype
     image_run $run_directory $run_name $plot_directory $snapshot_name $catalogue_name $ptype
   fi
 }
 
-ls "Runs" | grep Run | parallel -n1 -j56 "plot_single_run ${path}"
+#ls "Runs" | grep Run | parallel -n1 -j56 "plot_single_run ${path}"
